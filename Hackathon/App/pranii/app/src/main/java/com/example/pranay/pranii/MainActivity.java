10429508dac68af4785d4a8946fc7b0664220e92
unknown <pranay2410@gmail.com>
2019-01-13 06:57:57 -0800

    first commit

---
+++ Hackathon/App/pranii/app/src/main/java/com/example/pranay/pranii/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pranay.pranii;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
